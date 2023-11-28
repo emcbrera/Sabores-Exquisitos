8dcd80243656b46ced4b372a24ebc383323e5e9c
eimy <emcabrera@estudiante.uniajc.edu.co>
2023-11-28 00:26:45 -0500

    quinta

--- js/vistatwo.js
+++ js/vistatwo.js
@@ -1,7 +1,41 @@
 const data =[
-
     {
+        nombre: "Dulce amanecer",
+        precio: "$120.000",
+        seccion: "Cumpleaños",
+        categoria: "Golden",
+        genero: "Mujer" ,
+        contenido: "Omelette, milo caliente, chocolatina M&M, porción de torta, yougurt alpina", 
+        elementosor: "Tarjeta personalizada",
+        envio: "35% Descuento",
+        tamaño: "l20 cm x 40 cm",
+        personalizado: "No",
+        img: " ",
         
+    },
+    {
+        nombre: "",
+        precio: "",
+        seccion: "",
+        categoria: "",
+        genero: "" ,
+        contenido: "", 
+        elementosor: "",
+        envio: "",
+        tamaño: "",
+        personalizado: "",
+        img: "",
     }
 
 ]
+
+
+function anterior(){
+
+}
+
+function siguiente(){
+    if () {
+        
+    }
+}
